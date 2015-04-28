256c46b484ba25974754d94297abf7b6ade2f24c
Min You <my2248@c2b2.columbia.edu>
2015-04-28 18:56:16 -0400

    keep consist with running version

--- msviper_starter.r
+++ msviper_starter.r
@@ -5,6 +5,8 @@ exprsFile <- paste(runDir, args[2], sep="")
 adjFile <- paste(runDir, args[3], sep="")
 phenoFile <- paste(runDir, args[4], sep="")
 context <- args[5]
+caseGroups <- args[6]
+controlGroups <- args[7]
 gesFilter <- args[8]
 minSize <- args[9]
 bootStrapping <- args[10]
@@ -22,14 +24,13 @@ minimalSet <- ExpressionSet(assayData=exprs)
 pData <- read.table(phenoFile, row.names=1, header=TRUE, sep="\t")
 all(rownames(pData)==colnames(exprs))
 metadata <- data.frame(labelDescription=context)
+print(metadata)
 phenoData <- new("AnnotatedDataFrame", data=pData, varMetadata=metadata)
+print(phenoData)
 dSet <- ExpressionSet(assayData=exprs, phenoData=phenoData)
+print(dSet)
 regul <- aracne2regulon(adjFile, dSet, verbose = FALSE)
 
-
-print(phenoData)
-print(dSet)
-print(regul)
 print(bootStrapping)
 
 test1 <- c(unlist(strsplit(caseGroups, ",")))
@@ -38,20 +39,21 @@ test2 <- c(unlist(strsplit(controlGroups, ",")))
 {
 if (bootStrapping == "TRUE")
 {
-
+   print(context)
+   signature <- bootstrapTtest(dSet, context, test1, test2, verbose = FALSE)
+   
 
 }
 else
 {
    signature <- rowTtest(dSet, context, test1,  test2)
    signature <- (qnorm(signature$p.value/2, lower.tail = FALSE) * sign(signature$statistic))[, 1]
-
+   
 }
 }
 
 nullmodel <- ttestNull(dSet, context, test1, test2, per = 1000, repos = TRUE, verbose = FALSE)
-mrs <- msviper(signature, regul, nullmodel, minsize = as.numeric(minSize), ges.filter = as.logical(gesFilter=="TRUE"), verbose
-= FALSE)
+mrs <- msviper(signature, regul, nullmodel, minsize = as.numeric(minSize), ges.filter = as.logical(gesFilter=="TRUE"), verbose = FALSE)
 print(mrs)
 print(minSize)
 print(gesFilter)
@@ -77,16 +79,15 @@ if ( shadow == "TRUE" )
 }
 else
 {
-   if (class(signature) == "matrix") signature <- rowMeans(signature)
+   if (class(signature) == "matrix") signature <- rowMeans(signature)   
    write.table(signature, file=paste(runDir, "signature.txt", sep=""), sep = "\t")
    slist <- mrs$signature
    if (ncol(slist)>0) slist <- rowMeans(slist)
-   write.table(slist, file=paste(runDir, "mrsSignature.txt", sep=""), sep = "\t")
+   write.table(slist, file=paste(runDir, "mrsSignature.txt", sep=""), sep = "\t")   
    write.table(summary(mrs, length(mrs$regul)), file=paste(runDir, "result.txt", sep=""), sep="\t")
    write(paste(mrs_le$ledge, sep="\t"), file=paste(runDir, "ledges.txt",sep=""), sep="\t")
    write(names(mrs_le$ledge), file=paste(runDir, "masterRegulons.txt", sep=""), sep="\t")
-   write(paste(sapply(mrs$regulon, function(x) {names(x$tfmode)}), sep="\t"), file = paste(runDir, "regulons.txt", sep=""), sep
- = "\t")
+   write(paste(sapply(mrs$regulon, function(x) {names(x$tfmode)}), sep="\t"), file = paste(runDir, "regulons.txt", sep=""), sep = "\t")  
 }
 }
 
